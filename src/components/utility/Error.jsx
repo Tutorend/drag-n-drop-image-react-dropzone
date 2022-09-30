dc651e71a16b1820831e47fd4ce7242bf8582e5b
Tutorend <codetkcode@gmail.com>
2022-09-30 05:57:58 +0000

    Update package-lock.json and Error.jsx

--- src/components/utility/Error.jsx
+++ src/components/utility/Error.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 function Error({errorM}) {
-    if(errorM.code=="file-invalid-type"){
+    if(errorM.code==="file-invalid-type"){
         return <p className='text-xl text-red-400'>😔File Not Valid choose from .png .jpg or .jpeg</p>
     }
-    if(errorM.code == "too-many-files"){
+    if(errorM.code ==="too-many-files"){
         return <p className='text-xl text-red-400'>😔Maximum two files are allowed</p>
     }
  
